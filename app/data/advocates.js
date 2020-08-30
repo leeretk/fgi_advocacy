bc5b0f8e282197fec92bd49abd74941e9999c29e
Kamy <kamy.leeret@gmail.com>
2020-08-30 10:44:06 -0600

    Survey is working with new questions and answers
    need to incorporate photo's and descriptions for each advocate.

--- app/data/advocates.js
+++ app/data/advocates.js
@@ -3,74 +3,39 @@
 // *********************************************************************************
 var advocatesData = [
     {
-        name: 'norwegian-ridgeback',
+        name: 'Outspoken Advocate',
         email: 'kamy@leeret.com',
         photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/1norwegian-ridgeback.jpg',
-        scores: [4, 5, 2, 3, 1, 3, 1, 1, 5, 2],
+        scores: [1, 2, 2, 1, 2, 1, 2, 1, 2, 1],
         description: 'Hunter with a venomous tongue.'
     },  
     {
-        name: 'chinese-fireball',
+        name: 'Powerful Writer Advocate',
         email: 'kamy@leeret.com',
         photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/2_chinese-fireball.jpg',
-        scores: [5, 3, 4, 1, 5, 2, 5, 1, 2, 2],
+        scores: [2, 1, 2, 3, 1, 1, 1, 2, 2, 2],
         description: 'Easily angered, clever and agile.'
     },
     {
-        name: 'ukranian-ironbelly',
+        name: 'Group Leader / Organizing Advocate',
         email: 'kamy@leeret.com',
         photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/3_ukranian-ironbelly.jpg',
-        scores: [3, 2, 2, 2, 2, 2, 3, 1, 5, 4],
+        scores: [3, 4, 1, 1, 3, 1, 2, 2, 3, 1],
         description: 'Slow but tough as nails.'
     },
     {
-        name: 'hungarian-horntail',
+        name: 'Collaborative Advocate',
         email: 'kamy@leeret.com',
         photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/4_hungarian-horntail.jpg',
-        scores: [4, 3, 3, 3, 2, 4, 4, 1, 5, 4],
+        scores: [4, 4, 3, 2, 3, 2, 2, 2, 1, 1],
         description: 'Very dangerous, agile, and tempermental.'
     },
     {
-        name: 'smaug',
+        name: 'Supportive Advocate',
         email: 'kamy@leeret.com',
         photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/5_smaug.jpg',
-        scores: [5, 5, 5, 5, 5, 2, 4, 2, 2, 3],
+        scores: [4, 3, 5, 2, 1, 2, 2, 2, 1, 2],
         description: 'Psychopathic, sadistic, violent, arrogant, intelligent and greedy.'
     },
-    {
-        name: 'night-fury',
-        email: 'kamy@leeret.com',
-        photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/6_night-fury.jpg',
-        scores: [3, 3, 1, 2, 3, 5, 4, 5, 1, 5],
-        description: 'Rare, stealthy, intelligent and can be very dangerous.'
-    },
-    {
-        name: 'puff-the-magic-dragon',
-        email: 'kamy@leeret.com',
-        photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/7_puff-magic.jpg',
-        scores: [2, 1, 2, 3, 3, 4, 2, 4, 5, 3],
-        description: 'Has overcome childhood hardships, eclectic, free spirit.', 
-    },
-    {
-        name: 'petes-dragon',
-        email: 'kamy@leeret.com',
-        photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/8_petes-dragon.jpg',
-        scores: [2, 1, 1, 4, 4, 1, 4, 4, 1, 3],
-        description: 'Protective, playful, mischievous, and a loyal Advocate.' 
-    },
-    {
-        name: 'drogon',
-        email: 'kamy@leeret.com',
-        photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/9_drogon.jpg',
-        scores: [5, 5, 5, 1, 1, 1, 2, 4, 3, 4],
-        description: 'Protective and loyal but can be very destructive.'
-    },
-    {
-        name: 'mushu',
-        email: 'kamy@leeret.com',
-        photo: 'https://raw.githubusercontent.com/leeretk/AdvocateFinder/master/app/assets/images/10_mushu.jpg',
-        scores: [1, 1, 3, 5, 4, 5, 2, 4, 1, 2],
-        description: 'Selfish at times, overconfident and impulsive. Sassy but caring and protecive.',
-        },
 ];
 module.exports = advocatesData;
